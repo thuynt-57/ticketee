d5f2f6eaddd89e4b1acf5ed584cd61add8052396
thuynt_57 <thuynt_57@vnu.edu.vn>
2014-03-07 19:39:04 +0700

    Add title functionality for show page.

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -1,25 +1,3 @@
 require 'spec_helper'
 
-#describe ProjectsController do
-#
-#end
-class ProjectsController <ApplicationController
-	def index
-	end
-
-	def new
-		@project = Project.new
-	end
-
-	def create
-
-	end
-
-	def show
-      # @project = Project.find(params[:id])
-    end
-
-
-	
-end
 
